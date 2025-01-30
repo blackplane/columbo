1a1a29f35e00c845207d41839af438a025721d44
Armin <blackplane@tuta.io>
2025-01-30 08:16:19 -0800

    next

--- columbo/toxicity.py
+++ columbo/toxicity.py
@@ -24,8 +24,9 @@ def build_embedding(model_path: str = 'Alibaba-NLP/gte-base-en-v1.5', device: st
 
 def embed(text: str, tokenizer: Callable, embedder: Callable, normalize=True, device: str = "cpu"):
     batch_dict = tokenizer(text, max_length=8192, padding=True, truncation=True, return_tensors='pt').to(device)
+    batch_dict = {k: v.to(device) for k, v in batch_dict.items()}
     for k, v in batch_dict.items():
-        v.to(device)
+        print(k, v.device)
     outputs = embedder(**batch_dict)
     embedding = outputs.last_hidden_state[:, 0]
     if normalize:
